f8bec7134d3c0e5ef9891948c89a429451b9e7b1
Razvan <razvan@razshare.dev>
2025-04-22 11:54:31 +0200

    chore(app): formatting templates.

--- templates/api/example.go
+++ templates/api/example.go
@@ -4,10 +4,16 @@ import f "github.com/razshare/frizzante"
 
 func api(
 	withPattern func(pattern string),
-	withHandler func(handler func(request *f.Request, response *f.Response)),
+	withHandler func(handler func(
+		request *f.Request,
+		response *f.Response,
+	)),
 ) {
 	withPattern("GET /")
-	withHandler(func(request *f.Request, response *f.Response) {
+	withHandler(func(
+		request *f.Request,
+		response *f.Response,
+	) {
 		// Handle.
 		f.SendEcho(response, "Ok.")
 	})
--- templates/pages/example.go
+++ templates/pages/example.go
@@ -5,15 +5,31 @@ import f "github.com/razshare/frizzante"
 func page(
 	withPath func(path string),
 	withView func(view *f.View),
-	withBaseHandler func(baseHandler func(request *f.Request, response *f.Response, view *f.View)),
-	withActionHandler func(actionHandler func(request *f.Request, response *f.Response, view *f.View)),
+	withBaseHandler func(baseHandler func(
+		request *f.Request,
+		response *f.Response,
+		view *f.View,
+	)),
+	withActionHandler func(actionHandler func(
+		request *f.Request,
+		response *f.Response,
+		view *f.View,
+	)),
 ) {
 	withPath("/path")
 	withView(f.ViewReference("viewName"))
-	withBaseHandler(func(request *f.Request, response *f.Response, view *f.View) {
+	withBaseHandler(func(
+		request *f.Request,
+		response *f.Response,
+		view *f.View,
+	) {
 		// Show page.
 	})
-	withActionHandler(func(request *f.Request, response *f.Response, view *f.View) {
+	withActionHandler(func(
+		request *f.Request,
+		response *f.Response,
+		view *f.View,
+	) {
 		// Modify state.
 	})
 }
