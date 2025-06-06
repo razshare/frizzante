fdcebda2fefca38d23cee98b3db726da60dae27e
Razvan <razvan@razshare.dev>
2025-06-06 22:33:34 +0200

    lib(utilities): removing method from action, inferring it instead from the form itself.

--- cli/utilities/scripts/swaps.ts
+++ cli/utilities/scripts/swaps.ts
@@ -2,11 +2,11 @@ import type {View} from "../types.ts";
 import {uuid} from './uuid.ts'
 
 type SwapAction = {
-    method: () => "GET" | "POST"
+    method: () => string
     path: () => string
     body: () => unknown
     position: () => number
-    withMethod: (method: "GET" | "POST") => SwapAction
+    withMethod: (method: string) => SwapAction
     withPath: (path: string) => SwapAction
     withBody: (body: FormData) => SwapAction
     play: (update: boolean) => Promise<void>
@@ -20,7 +20,7 @@ function find(id: string): false | SwapAction {
 }
 
 function swap(view: View<unknown>): SwapAction {
-    let swapMethod = 'GET' as "GET" | "POST"
+    let swapMethod = 'GET'
     let swapPath = location.pathname
     let swapBody: FormData
     const swapPosition = nextPosition++
@@ -38,7 +38,7 @@ function swap(view: View<unknown>): SwapAction {
         position() {
             return swapPosition
         },
-        withMethod(method: "GET" | "POST") {
+        withMethod(method: string) {
             swapMethod = method
             return this
         },
