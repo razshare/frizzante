0c9d54b899051009368d9e705e47c225bb5ee592
Razvan <razvan@razshare.dev>
2025-06-28 22:26:53 +0200

    chore(app): project template is now embedded.

--- main.go
+++ main.go
@@ -3,65 +3,39 @@ package main
 import (
 	"embed"
 	"fmt"
-	"github.com/razshare/frizzante/fs"
+	ffs "github.com/razshare/frizzante/fs"
 	flag "github.com/spf13/pflag"
+	"io"
+	"io/fs"
 	"log"
 	"os"
-	"path/filepath"
 )
 
-const binaryVersion = "v1.8.1"
-const projectTemplateVersion = "v1.2.6"
+const binaryVersion = "v1.8.2"
 
 var FlagHelp = flag.BoolP("help", "h", false, "shows the help document")
 var FlagVersion = flag.BoolP("version", "v", false, "shows the binary version and the project version")
-var FlagCreateProject = flag.StringP("create-project", "c", "", fmt.Sprintf("creates a frizzante project (%s) to a directory", projectTemplateVersion))
-var FlagRestoreUtilities = flag.StringP("restore-utilities", "r", "", fmt.Sprintf("restores frizzante utilities (%s) to a directory", projectTemplateVersion))
+var FlagCreateProject = flag.StringP("create-project", "c", "", fmt.Sprintf("creates a frizzante project"))
+var FlagRestoreUtilities = flag.StringP("restore-utilities", "r", "", fmt.Sprintf("restores frizzante utilities"))
 
 //go:embed app/lib/utilities
-var utilities embed.FS
+//go:embed project.zip
+var embedded embed.FS
 
 func main() {
 	flag.Parse()
 
-	if *FlagCreateProject != "" {
-		downloadError := fs.DownloadFile(fmt.Sprintf("https://github.com/razshare/frizzante-starter/archive/refs/tags/%s.zip", projectTemplateVersion), *FlagCreateProject+".zip")
-		if downloadError != nil {
-			log.Fatal(downloadError)
-		}
-
-		unzipError := fs.UnzipFile(*FlagCreateProject+".zip", "."+*FlagCreateProject+".tmp")
-		if unzipError != nil {
-			log.Fatal(unzipError)
-		}
-
-		removeError := os.Remove(*FlagCreateProject + ".zip")
-		if removeError != nil {
-			log.Fatal(removeError)
-		}
-
-		renameError := os.Rename(filepath.Join("."+*FlagCreateProject+".tmp", "frizzante-starter-1.2.5"), *FlagCreateProject)
-		if renameError != nil {
-			log.Fatal(renameError)
-		}
-
-		removeError = os.RemoveAll("." + *FlagCreateProject + ".tmp")
-		if removeError != nil {
-			log.Fatal(removeError)
-		}
-	}
-
 	if *FlagRestoreUtilities != "" {
 		var restore func(from string, to string)
 		restore = func(from string, to string) {
-			if !fs.IsDirectory(to) {
+			if !ffs.IsDirectory(to) {
 				mkdirAllError := os.MkdirAll(to, os.ModePerm)
 				if mkdirAllError != nil {
 					log.Fatal(mkdirAllError)
 				}
 			}
 
-			entries, readDirError := utilities.ReadDir(from)
+			entries, readDirError := embedded.ReadDir(from)
 			if readDirError != nil {
 				log.Fatal(readDirError)
 			}
@@ -77,7 +51,7 @@ func main() {
 				fromFileName := from + "/" + entry.Name()
 				toFileName := to + "/" + entry.Name()
 
-				data, readError := utilities.ReadFile(fromFileName)
+				data, readError := embedded.ReadFile(fromFileName)
 				if readError != nil {
 					log.Fatal(readError)
 				}
@@ -94,12 +68,52 @@ func main() {
 	}
 
 	if *FlagVersion {
-		fmt.Printf("%s using project %s\n", binaryVersion, projectTemplateVersion)
+		println(binaryVersion)
 		os.Exit(0)
 	}
 
 	if *FlagHelp {
 		flag.Usage()
+		os.Exit(0)
+	}
+
+	if *FlagCreateProject != "" {
+		srcFile, srcError := embedded.Open("project.zip")
+		if srcError != nil {
+			log.Fatal(srcError)
+		}
+		defer func(srcFile fs.File) {
+			err := srcFile.Close()
+			if err != nil {
+				log.Fatal(err)
+			}
+		}(srcFile)
+
+		destFile, destError := os.Create(*FlagCreateProject + ".zip")
+		if destError != nil {
+			log.Fatal(destError)
+		}
+		defer func(destFile *os.File) {
+			err := destFile.Close()
+			if err != nil {
+				log.Fatal(err)
+			}
+		}(destFile)
+
+		_, copyError := io.Copy(destFile, srcFile)
+		if copyError != nil {
+			return
+		}
+
+		unzipError := ffs.UnzipFile(*FlagCreateProject+".zip", *FlagCreateProject)
+		if unzipError != nil {
+			log.Fatal(unzipError)
+		}
+
+		removeError := os.Remove(*FlagCreateProject + ".zip")
+		if removeError != nil {
+			log.Fatal(removeError)
+		}
 		os.Exit(0)
 	}
 
