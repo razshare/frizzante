5b508d7b23f08feb3cf966ac5f6e6c613fa4e0b0
Razvan <razvan@razshare.dev>
2025-07-24 04:55:12 +0200

    chore(app): tagging version v1.12.3

--- sessions/types.go
+++ sessions/types.go
@@ -1,14 +1,8 @@
 package sessions
 
-import (
-	"github.com/razshare/frizzante/archives"
-	"github.com/razshare/frizzante/connections"
-)
-
-type SessionStarter[T any] = func(con *connections.Connection, state T) *Session[T]
+import "github.com/razshare/frizzante/connections"
 
 type Session[T any] struct {
-	Archive    archives.Archive
 	Connection *connections.Connection
 	State      *T
 }
