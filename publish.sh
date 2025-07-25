5106da612a6e9e243ada55d9f19d524a1d68d9c3
Razvan <razvan@razshare.dev>
2025-07-25 03:56:44 +0200

    chore(app): tagging version v1.14.6

--- publish.sh
+++ publish.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2155
 export frizzante_version=$(< version)
-git add .
-git commit -m"chore(app): tagging version $frizzante_version"
-git tag "$frizzante_version"
-git push --tags
+make test && \
+git add . && \
+git commit -m"chore(app): tagging version $frizzante_version" && \
+git tag "$frizzante_version" && \
+git push --tags && \
 git push
