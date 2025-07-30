e9120b83a21b5d772f689e48c53ff350b1905a98
Razvan <razvan@razshare.dev>
2025-07-30 08:14:48 +0200

    chore(app): adding queries.sql and schema.sql to the cli's efs.

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 //go:embed version
 //go:embed app/frizzante
 //go:embed sqlc.yaml
+//go:embed queries.sql
+//go:embed schema.sql
 var cliEfs embed.FS
 var cli = frizzanteCli.Cli{Efs: cliEfs}
 
