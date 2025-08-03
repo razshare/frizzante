31099a9064c786219acf6b041a02b3a1bb52df07
Razvan <razvan@razshare.dev>
2025-08-03 16:26:00 +0200

    chore(app): tagging version v1.16.19

--- views_test.go
+++ views_test.go
@@ -33,7 +33,7 @@ func TestRenderServer(test *testing.T) {
 	}
 }
 
-func TestBenchmarkRenderServer(b *testing.T) {
+func BenchmarkRenderServer(b *testing.B) {
 	count := 10_000
 	var group sync.WaitGroup
 	group.Add(count)
