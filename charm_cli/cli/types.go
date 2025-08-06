c3a96462aa964562293637deaf90791dad0c36e4
Razvan <razvan@razshare.dev>
2025-08-06 23:45:19 +0200

    feat(charm_cli): removing unused platform type.

--- charm_cli/cli/types.go
+++ charm_cli/cli/types.go
@@ -8,15 +8,6 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-type Platform uint
-
-const PlatformLinuxAmd64 Platform = 0
-const PlatformLinuxArm64 Platform = 1
-const PlatformDarwinAmd64 Platform = 2
-const PlatformDarwinArm64 Platform = 3
-const PlatformWindowsAmd64 Platform = 4
-const PlatformWindowsArm64 Platform = 5
-
 type FeatureCopyInstruction struct {
 	FeatureName          string
 	OriginDirectory      string
