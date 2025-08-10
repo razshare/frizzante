8f63211f3bbd00bbe3171b5604c55eeaf936d011
Razvan <razvan@razshare.dev>
2025-08-10 09:29:32 +0200

    chore(app): tagging version v1.17.10

--- tui/table/options.go
+++ tui/table/options.go
@@ -8,14 +8,14 @@ import (
 func DefaultOptions() Options {
 	return Options{
 		MaxColumnWidth: 60,
-		ColumnPadding: 2,
-		HeaderHeight: 2,
+		ColumnPadding:  2,
+		HeaderHeight:   2,
 		HeaderStyle: lipgloss.NewStyle().
 			Bold(true).
 			Foreground(lipgloss.Color("221")),
-		RowStyle:    lipgloss.NewStyle().
+		RowStyle: lipgloss.NewStyle().
 			Foreground(lipgloss.Color(config.Colors.Primary)),
 		AltRowStyle: lipgloss.NewStyle().
-			Foreground(lipgloss.Color(config.Colors.Info)),
+			Foreground(lipgloss.Color(config.Colors.Secondary)),
 	}
 }
