5dfa6684436d9489ffe9e11a1f2a77f1691413ae
Razvan <razvan@razshare.dev>
2025-08-10 09:43:49 +0200

    chore(app): tagging version v1.17.14

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@ export frizzante_version=$(< version)
 #export frizzante_message=$(sed '1d;' version)
 git add . && \
 git commit -m"chore(app): tagging version $frizzante_version" && \
-git push
+git push && \
 git tag "$frizzante_version" && \
 git push --tags && \
 go install "github.com/razshare/frizzante@$frizzante_version"
