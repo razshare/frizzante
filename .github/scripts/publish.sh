c880934a0b582181863e3cda18c6d40f418128b3
Razvan <razvan@razshare.dev>
2025-08-26 14:27:34 +0200

    chore(app): updating workflow.

--- .github/scripts/publish.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-./.github/scripts/000.create_identity.sh && \
-./.github/scripts/100.fix_version.sh && \
-./.github/scripts/200.run_tests.sh && \
-./.github/scripts/300.create_zip.sh && \
-./.github/scripts/400.create_tag.sh && \
-./.github/scripts/500.save_changes.sh
