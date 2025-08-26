1190ce683d2b4177c26ca8e167e6f7035d878aee
Razvan <razvan@razshare.dev>
2025-08-26 13:17:35 +0200

    chore(app): updating workflow.

--- .github/scripts/200.run_tests.sh
+++ .github/scripts/200.run_tests.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-make clean configure package sync && go test -short ./...
+make clean && \
+go run main.go \
+  --app="template/project/app" \
+  --go="go" \
+  --bun="bun" \
+  --configure && \
+make sync && \
+make test
