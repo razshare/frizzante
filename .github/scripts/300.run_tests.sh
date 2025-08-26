14fc1a24da83da15157c40a8a5ad44e5ee2ef396
Razvan <razvan@razshare.dev>
2025-08-26 14:34:14 +0200

    chore(app): updating workflow.

--- .github/scripts/300.run_tests.sh
+++ .github/scripts/300.run_tests.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
-make clean configure package sync && \
-go test . && \
-go test ./embeds && \
-go test ./files && \
-go test ./js && \
-go test ./js/runtime && \
-go test ./mime && \
-go test ./receive && \
-go test ./send && \
-go test ./server && \
+set -o pipefail
+
+make clean configure package sync
+
+go test .
+
+go test ./embeds & \
+go test ./files & \
+go test ./js & \
+go test ./js/runtime & \
+go test ./mime & \
+go test ./receive & \
+go test ./send & \
+go test ./server & \
 go test ./svelte/ssr
