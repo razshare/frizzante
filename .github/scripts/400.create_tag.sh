118fc59db1f27c3234c801dc26fe2fe03e37e4be
Razvan <razvan@razshare.dev>
2025-08-31 18:18:22 +0200

    chore(app): updating workflow scripts.

--- .github/scripts/400.create_tag.sh
+++ .github/scripts/400.create_tag.sh
@@ -2,8 +2,8 @@
 # shellcheck disable=SC2155
 export frizzante_version=$(< version)
 git add . && \
-#git commit -m"chore(app): tagging version $frizzante_version" && \
-#git tag "$frizzante_version"
+git commit -m"chore(app): tagging version $frizzante_version" && \
+git tag "$frizzante_version"
 make lock
 git add .
 git commit -m"chore(app): locking template"
