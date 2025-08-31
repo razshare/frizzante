8b0da63f6dac10b803c3f75770c811f63b1c2e7f
Razvan <razvan@razshare.dev>
2025-09-01 01:23:46 +0200

    chore(app): bump.

--- publish.sh
+++ publish.sh
@@ -17,16 +17,19 @@ go mod tidy
 ../../frizzante --test
 popd
 
-# Removes temporary binary
+# Deletes temporary binary
 rm -f frizzante
+
+# Deletes previous binaries
+rm -fr .gen/bin
 
 # Builds binaries
 GOOS="linux" GOARCH="amd64" go build -o .gen/bin/frizzante-linux-amd64 & \
 GOOS="linux" GOARCH="arm64" go build -o .gen/bin/frizzante-linux-arm64 & \
 GOOS="darwin" GOARCH="amd64" go build -o .gen/bin/frizzante-darwin-amd64 & \
 GOOS="darwin" GOARCH="arm64" go build -o .gen/bin/frizzante-darwin-arm64 & \
-GOOS="windows" GOARCH="amd64" go build -o .gen/bin/frizzante-windows-amd64 & \
-GOOS="windows" GOARCH="arm64" go build -o .gen/bin/frizzante-windows-arm64 & \
+GOOS="windows" GOARCH="amd64" go build -o .gen/bin/frizzante-windows-amd64.exe & \
+GOOS="windows" GOARCH="arm64" go build -o .gen/bin/frizzante-windows-arm64.exe & \
 wait
 
 # Creates binaries archives
@@ -34,8 +37,8 @@ zip -rjq9 .gen/bin/frizzante-linux-amd64.zip .gen/bin/frizzante-linux-amd64 & \
 zip -rjq9 .gen/bin/frizzante-linux-arm64.zip .gen/bin/frizzante-linux-arm64 & \
 zip -rjq9 .gen/bin/frizzante-darwin-amd64.zip .gen/bin/frizzante-darwin-amd64 & \
 zip -rjq9 .gen/bin/frizzante-darwin-arm64.zip .gen/bin/frizzante-darwin-arm64 & \
-zip -rjq9 .gen/bin/frizzante-windows-amd64.zip .gen/bin/frizzante-windows-amd64 & \
-zip -rjq9 .gen/bin/frizzante-windows-arm64.zip .gen/bin/frizzante-windows-arm64 & \
+zip -rjq9 .gen/bin/frizzante-windows-amd64.zip .gen/bin/frizzante-windows-amd64.exe & \
+zip -rjq9 .gen/bin/frizzante-windows-arm64.zip .gen/bin/frizzante-windows-arm64.exe & \
 wait
 
 # Deletes binaries
@@ -43,8 +46,8 @@ rm -f .gen/bin/frizzante-linux-amd64 & \
 rm -f .gen/bin/frizzante-linux-arm64 & \
 rm -f .gen/bin/frizzante-darwin-amd64 & \
 rm -f .gen/bin/frizzante-darwin-arm64 & \
-rm -f .gen/bin/frizzante-windows-amd64 & \
-rm -f .gen/bin/frizzante-windows-arm64 & \
+rm -f .gen/bin/frizzante-windows-amd64.exe & \
+rm -f .gen/bin/frizzante-windows-arm64.exe & \
 wait
 
 # Opens file explorer and browser
