43b45482b088579992c37489eb349b7849237d27
Razvan <razvan@razshare.dev>
2025-09-20 21:03:02 +0200

    chore(app): bump.

--- internal/project/main.go
+++ internal/project/main.go
@@ -14,9 +14,9 @@ import (
 	"github.com/razshare/frizzante/internal/project/lib/routes/handlers/welcome"
 )
 
-//go:generate make types
 //go:generate make clean configure
 //go:generate make package
+//go:generate make types
 //go:embed app/dist
 var efs embed.FS
 var srv = server.New()
