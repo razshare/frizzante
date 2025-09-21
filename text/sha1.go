c227c290fa01ded70107e024ca8e605c86ebf01d
Razvan <razvan@razshare.dev>
2025-09-21 15:09:45 +0200

    feat(text): inlining sha1 string length.

--- text/sha1.go
+++ text/sha1.go
@@ -8,7 +8,7 @@ import (
 func Sha1(text string) string {
 	from := sha1.Sum([]byte(text))
 	to := make([]byte, 20)
-	for i := len(to) - 1; i >= 0; i-- {
+	for i := 19; i >= 0; i-- {
 		to[i] = from[i]
 	}
 	return hex.EncodeToString(to)
