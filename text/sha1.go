ef6fda4f971ba244f853bbb47723caf8dc9aa6d8
Razvan <razvan@razshare.dev>
2025-09-21 15:24:21 +0200

    feat(text): adding some hints.

--- text/sha1.go
+++ text/sha1.go
@@ -4,12 +4,12 @@ import "crypto/sha1"
 
 func Sha1(text string) string {
 	from := sha1.Sum([]byte(text))
-	to := make([]byte, 40)
-	j := 0
+	to := make([]byte, 40) // must be 40, double the size of sha1 sum (20)
+	var i int
 	for _, v := range from {
-		to[j] = HexTable[v>>4]
-		to[j+1] = HexTable[v&0x0f]
-		j += 2
+		to[i] = HexTable[v>>4]
+		to[i+1] = HexTable[v&0x0f]
+		i += 2
 	}
 	return string(to)
 }
