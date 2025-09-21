c85c434e1ba11f24f845e40e0f80cf0e71b332ef
Razvan <razvan@razshare.dev>
2025-09-21 15:33:01 +0200

    feat(text): adding some more comments to Sha1 encoding.

--- text/sha1.go
+++ text/sha1.go
@@ -7,9 +7,9 @@ func Sha1(text string) string {
 	to := make([]byte, 40) // must be 40, double the size of sha1 sum (20)
 	var i int
 	for _, v := range from {
-		to[i] = HexTable[v>>4]
-		to[i+1] = HexTable[v&0x0f]
-		i += 2
+		to[i] = HexTable[v>>4]     // encoding the first 4 bits, first nibble
+		to[i+1] = HexTable[v&0x0f] // encoding the second 4 bits, second nibble
+		i += 2                     // jump by 2 because we encoded 2 nibbles
 	}
 	return string(to)
 }
