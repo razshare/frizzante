4684b2d6326b24c40f36e430bcc321f6a0a823b8
Razvan <razvan@razshare.dev>
2025-09-21 15:22:13 +0200

    feat(text): inlining sha1 hex encoding.

---
+++ text/constants.go
@@ -0,0 +1,3 @@
+package text
+
+const HexTable = "0123456789abcdef"
--- text/sha1.go
+++ text/sha1.go
@@ -1,15 +1,15 @@
 package text
 
-import (
-	"crypto/sha1"
-	"encoding/hex"
-)
+import "crypto/sha1"
 
 func Sha1(text string) string {
 	from := sha1.Sum([]byte(text))
-	to := make([]byte, 20)
-	for i := 19; i >= 0; i-- {
-		to[i] = from[i]
+	to := make([]byte, 40)
+	j := 0
+	for _, v := range from {
+		to[j] = HexTable[v>>4]
+		to[j+1] = HexTable[v&0x0f]
+		j += 2
 	}
-	return hex.EncodeToString(to)
+	return string(to)
 }
