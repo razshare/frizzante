7704f4a844ce1e51a9bd60468a44d8f958e8a44f
Razvan <razvan@razshare.dev>
2025-09-21 15:34:05 +0200

    feat(text): clarifying Sha1 comments.

--- text/sha1.go
+++ text/sha1.go
@@ -7,8 +7,8 @@ func Sha1(text string) string {
 	to := make([]byte, 40) // must be 40, double the size of sha1 sum (20)
 	var i int
 	for _, v := range from {
-		to[i] = HexTable[v>>4]     // encoding the first 4 bits, first nibble
-		to[i+1] = HexTable[v&0x0f] // encoding the second 4 bits, second nibble
+		to[i] = HexTable[v>>4]     // encoding the left half, first nibble
+		to[i+1] = HexTable[v&0x0f] // encoding the right half, second nibble
 		i += 2                     // jump by 2 because we encoded 2 nibbles
 	}
 	return string(to)
