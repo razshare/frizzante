a39340aa6ad272122c5de1a92985f5eb4bcc6017
Razvan <razvan@razshare.dev>
2025-09-22 02:58:38 +0200

    chore(app): fixing tests.

--- internal/project/lib/core/receive/auth_test.go
+++ internal/project/lib/core/receive/auth_test.go
@@ -11,13 +11,9 @@ func TestBasicAuth(t *testing.T) {
 	client := mock.NewClient()
 	token := base64.URLEncoding.EncodeToString([]byte("test:123"))
 	client.Request.Header.Set("Authorization", "Basic "+token)
-	var username string
-	var password string
-	var ok bool
 
-	if username, password, ok = BasicAuth(client); !ok {
-		t.Fatal("auth should pass")
-	}
+	username, password := BasicAuth(client)
+
 	if username != "test" {
 		t.Fatal("user should be test")
 	}
