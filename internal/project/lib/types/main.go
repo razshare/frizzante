39c492584bc7696a93eafb8ebda5a29733710189
Razvan <razvan@razshare.dev>
2025-09-25 15:48:57 +0200

    feat(cli): removing types entry from lib/types/main.go.

--- internal/project/lib/types/main.go
+++ internal/project/lib/types/main.go
@@ -1,10 +1,4 @@
 package main
 
-import (
-	"github.com/razshare/frizzante/internal/project/lib/core/types"
-	"github.com/razshare/frizzante/internal/project/lib/routes/handlers/todos"
-)
-
 func main() {
-	types.Generate[todos.Props]()
 }
