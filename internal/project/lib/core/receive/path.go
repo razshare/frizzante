27eaa262d5b724b19b280fafb89e56d19b1a7021
Razvan <razvan@razshare.dev>
2025-09-25 16:29:59 +0200

    chore(docs): updating go docs.

--- internal/project/lib/core/receive/path.go
+++ internal/project/lib/core/receive/path.go
@@ -3,8 +3,6 @@ package receive
 import "github.com/razshare/frizzante/internal/project/lib/core/client"
 
 // Path reads a parameters fields and returns the value.
-//
-// Compatible with web sockets.
 func Path(client *client.Client, key string) string {
 	return client.Request.PathValue(key)
 }
