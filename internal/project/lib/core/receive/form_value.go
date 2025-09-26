03c64ce40d187afa3fe9c0fc069fb2adf853880b
Razvan <razvan@razshare.dev>
2025-09-26 11:55:59 +0200

    feat(core): form_value now correctly parses post verb when not multipart.

--- internal/project/lib/core/receive/form_value.go
+++ internal/project/lib/core/receive/form_value.go
@@ -1,6 +1,9 @@
 package receive
 
 import (
+	"errors"
+	"net/http"
+
 	"github.com/razshare/frizzante/internal/project/lib/core/client"
 	"github.com/razshare/frizzante/internal/project/lib/core/stack"
 )
@@ -9,12 +12,24 @@ import (
 func FormValue(client *client.Client, key string) string {
 	if client.Request.Form == nil {
 		if err := client.Request.ParseMultipartForm(MaxFormSize); err != nil {
+			if !errors.Is(err, http.ErrNotMultipart) {
+				return ""
+			}
+
+			err = client.Request.ParseForm()
+			if err != nil {
+				client.Config.ErrorLog.Println(err, stack.Trace())
+				return ""
+			}
+
 			client.Config.ErrorLog.Println(err, stack.Trace())
 			return ""
 		}
 	}
+
 	if vs := client.Request.Form[key]; len(vs) > 0 {
 		return vs[0]
 	}
+
 	return ""
 }
