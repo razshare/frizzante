dfc790fb3245458957f5cb6f96e4791220bd6906
Razvan <razvan@razshare.dev>
2025-10-07 14:33:25 +0200

    chore(app): fixing an issue where all view components would always be imported on first render.

--- internal/project/app/exports.client.ts
+++ internal/project/app/exports.client.ts
@@ -1,4 +1,4 @@
 export const views = {
-    Welcome: import("$lib/views/Welcome.svelte"),
-    Todos: import("$lib/views/Todos.svelte"),
+    Welcome: () => import("$lib/views/Welcome.svelte"),
+    Todos: () => import("$lib/views/Todos.svelte"),
 }
--- internal/project/app/lib/components/core/ClientRouter.svelte
+++ internal/project/app/lib/components/core/ClientRouter.svelte
@@ -4,13 +4,13 @@
     import Async from "$lib/components/core/Async.svelte"
     import type { View } from "$lib/scripts/core/types.js"
     let { name, props, render, align }: View<Record<string, unknown>> = $props()
-    const components = views as unknown as Record<string, Promise<SvelteComponent>>
+    const components = views as unknown as Record<string, ()=>Promise<SvelteComponent>>
     const view: View<Record<string, unknown>> = $state({ name, props, render, align })
     setContext("view", view)
 </script>
 
 {#each Object.keys(components) as key (key)}
     {#if key === view.name}
-        <Async from={components[key]} properties={view.props} />
+        <Async from={components[key]()} properties={view.props} />
     {/if}
 {/each}
