2cdd6e6015419468cfac22598942cba5b03353e2
Razvan <razvan@razshare.dev>
2025-10-12 04:24:44 +0200

    feat(core): fixing an inconsistency with the client router where the view would be destroyed then mounted again before navigating to a different view.
    This would also trigger transition animations, which makes it annoying.

--- cli/action/package_watch.go
+++ cli/action/package_watch.go
@@ -35,14 +35,14 @@ func PackageWatch(options PackageWatchOptions) (err error) {
 	group.Go(func() {
 		messages.Command(
 			options.App,
-			append(os.Environ(), "DEV=1"),
+			append(os.Environ(), "SOURCE_MAP=1"),
 			bun, "x", "vite", "build", "--logLevel=info", "--outDir=dist", "--emptyOutDir=false", "--watch", "--ssr=app.server.ts",
 		)
 	})
 	group.Go(func() {
 		messages.Command(
 			options.App,
-			append(os.Environ(), "DEV=1"),
+			os.Environ(),
 			filepath.Join("node_modules", ".bin", "esbuild"),
 			"--bundle", "--watch", "--outfile=dist/app.server.cjs", "--format=cjs", "--allow-overwrite", "dist/app.server.js",
 		)
