933c3b8678e6c7d4b6590c6024afd8980de6a8c4
Razvan <razvan@razshare.dev>
2025-10-14 07:21:12 +0200

    chore(app): removing some dead code.

--- internal/project/lib/routes/todos/types.go
+++ internal/project/lib/routes/todos/types.go
@@ -1,8 +1,6 @@
 package todos
 
 import (
-	"mime/multipart"
-
 	"github.com/razshare/frizzante/internal/project/lib/core/types"
 	"github.com/razshare/frizzante/internal/project/lib/sessions/memory"
 )
@@ -17,9 +15,8 @@ type Props struct {
 }
 
 type ToggleForm struct {
-	Index int                  `form:"index"`
-	Value int                  `form:"value"`
-	File  multipart.FileHeader `form:"file"`
+	Index int `form:"index"`
+	Value int `form:"value"`
 }
 
 type AddForm struct {
