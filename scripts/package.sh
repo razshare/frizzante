6df92c3d4d6dac369e42c606275715b1b3232800
Razvan <razvan@razshare.dev>
2025-10-14 06:10:04 +0200

    chore(app): improving package.sh.

--- scripts/package.sh
+++ scripts/package.sh
@@ -10,4 +10,6 @@ test -f frizzante || go build -o frizzante
 # Configures internal project
 pushd internal/project && \
 ../../frizzante --package && \
+../../frizzante -gtypes && \
+../../frizzante --check && \
 popd || exit 1
