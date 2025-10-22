7541346c5595f0959b6ce455fa686b8e483a6bfc
Razvan <razvan@razshare.dev>
2025-10-22 21:10:45 +0200

    feat(cli): reacting to KeyHome and KeyEnd.

--- tui/singleselect/model.go
+++ tui/singleselect/model.go
@@ -50,17 +50,42 @@ func (model *Model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
 			return model, nil
 		}
 
-		if assert.Type == tea.KeyPgDown {
-			navigate.Apply(model.Search, model.Viewport, model.Viewport.Visible)
-			return model, nil
-		}
-
 		if assert.Type == tea.KeyPgUp {
 			navigate.Apply(model.Search, model.Viewport, -model.Viewport.Visible)
 			return model, nil
 		}
 
-		if assert.Type == tea.KeyBackspace || assert.Type == tea.KeyCtrlH {
+		if assert.Type == tea.KeyPgDown {
+			navigate.Apply(model.Search, model.Viewport, model.Viewport.Visible)
+			return model, nil
+		}
+
+		if assert.Type == tea.KeyHome {
+			home := model.Viewport.Offset
+			if model.Viewport.Cursor == home {
+				navigate.Apply(model.Search, model.Viewport, -model.Viewport.Visible)
+			} else {
+				model.Viewport.Cursor = home
+				navigate.Apply(model.Search, model.Viewport, 0)
+			}
+			return model, nil
+		}
+
+		if assert.Type == tea.KeyEnd {
+			end := model.Viewport.Offset + model.Viewport.Visible - 1
+			if model.Viewport.Cursor == end {
+				navigate.Apply(model.Search, model.Viewport, model.Viewport.Visible)
+			} else {
+				model.Viewport.Cursor = end
+				navigate.Apply(model.Search, model.Viewport, 0)
+			}
+			return model, nil
+		}
+
+		// vscode has a weird bug where it will send a "ctrl+w" whenever the user presses "backspace" in the integrated terminal,
+		// so we're including tea.KeyCtrlW to try to fix that for the user.
+		// https://stackoverflow.com/questions/52806758/visual-studio-code-ctrlbackspace-not-working-in-integrated-terminal
+		if assert.Type == tea.KeyBackspace || assert.Type == tea.KeyCtrlH || assert.Type == tea.KeyCtrlW {
 			if model.Search.Active {
 				return model, search.Apply(model.Search, model.Viewport, assert)
 			}
