c0e9ff6c7cd1f2b0b0b023ad150e6fbeb5192fbb
Razvan <razvan@razshare.dev>
2025-11-10 04:00:59 +0100

    chore(app): updating tests.

--- cli/generate/download_test.go
+++ cli/generate/download_test.go
@@ -1,17 +1,56 @@
 package generate
 
 import (
+	"bytes"
+	"embed"
+	"io/fs"
+	"net/http"
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/razshare/frizzante/cli/paths"
 	"github.com/razshare/frizzante/internal/additions/lib/security"
+	"github.com/razshare/frizzante/internal/project/lib/core/clients"
 	"github.com/razshare/frizzante/internal/project/lib/core/files"
+	"github.com/razshare/frizzante/internal/project/lib/core/routes"
+	"github.com/razshare/frizzante/internal/project/lib/core/servers"
 )
 
+//go:embed test.zip
+var TestDownloadEfs embed.FS
+
 func TestDownload(t *testing.T) {
-	url := "https://github.com/razshare/frizzante/archive/refs/heads/main.zip"
+	server := servers.New()
+	server.Addr = "127.0.0.1:7676"
+	server.Routes = []routes.Route{
+		{Pattern: "GET /", Handler: func(client *clients.Client) {
+			var file fs.File
+			var err error
+			if file, err = TestDownloadEfs.Open("test.zip"); err != nil {
+				t.Fatal(err)
+			}
+
+			var info os.FileInfo
+			if info, err = file.Stat(); err != nil {
+				t.Fatal(err)
+			}
+
+			buf := make([]byte, info.Size())
+			if _, err = file.Read(buf); err != nil {
+				t.Fatal(err)
+			}
+
+			http.ServeContent(client.Writer, &client.Request, "test_out.zip", info.ModTime(), bytes.NewReader(buf))
+		}},
+	}
+	go servers.Start(server)
+	defer func() { server.Channels.End <- struct{}{} }()
+
+	time.Sleep(time.Second)
+
+	url := "http://127.0.0.1:7676/test_out.zip"
 
 	var err error
 
@@ -27,10 +66,10 @@ func TestDownload(t *testing.T) {
 	if err = os.RemoveAll(cached); err != nil {
 		t.Fatal(err)
 	}
-	if err = os.RemoveAll("main.zip"); err != nil {
+	if err = os.RemoveAll("test_out.zip"); err != nil {
 		t.Fatal(err)
 	}
-	defer func() { _ = os.RemoveAll("main.zip") }()
+	defer func() { _ = os.RemoveAll("test_out.zip") }()
 	defer func() { _ = os.RemoveAll(cached) }()
 
 	install, evict, err := Download(DownloadOptions{Auto: true, Url: url})
