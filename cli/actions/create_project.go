4bbfa7e576c474c680776b5e40448051b307ce6f
Razvan <razvan@razshare.dev>
2025-11-17 22:19:31 +0100

    feat(create project): no longer configuring project automatically.

--- cli/actions/create_project.go
+++ cli/actions/create_project.go
@@ -1,10 +1,7 @@
 package actions
 
 import (
-	"os"
-
 	"github.com/razshare/frizzante/cli/generate"
-	"github.com/razshare/frizzante/platforms"
 	"github.com/razshare/frizzante/tui/inputs"
 )
 
@@ -20,35 +17,6 @@ func CreateProject(options CreateProjectOptions) (err error) {
 		Name: options.Name,
 		Go:   options.Go,
 		Efs:  options.Efs,
-	}); err != nil {
-		return
-	}
-
-	if err = os.Chdir(options.Name); err != nil {
-		return
-	}
-
-	if err = Configure(ConfigureOptions{
-		Auto:     true,
-		Go:       options.Go,
-		Efs:      options.Efs,
-		Platform: platforms.Detect(),
-		Bun:      options.Bun,
-		Air:      options.Air,
-	}); err != nil {
-		return
-	}
-
-	if err = Install(InstallOptions{
-		Go:  options.Go,
-		Bun: options.Bun,
-	}); err != nil {
-		return
-	}
-
-	if err = Package(PackageOptions{
-		Bun:  options.Bun,
-		Prod: false,
 	}); err != nil {
 		return
 	}
