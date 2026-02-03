6e50b5ab46836c042c25e051340c08bdda76a9bd
Razvan <razvan@razshare.dev>
2026-02-03 03:52:44 +0100

    feat(dev): disable empty out dir during package watch mode, instead the out dirs are deleted once before package watch mode starts.

--- cli/actions/package_watch.go
+++ cli/actions/package_watch.go
@@ -9,13 +9,19 @@ import (
 )
 
 func PackageWatch(options PackageWatchOptions) (err error) {
+	if err = os.RemoveAll(filepath.Join("app", "dist", "client")); err != nil {
+		return
+	}
+	if err = os.RemoveAll(filepath.Join("app", "dist", "server")); err != nil {
+		return
+	}
 	var group sync.WaitGroup
 	group.Go(func() {
 		if !messages.Command(messages.CommandOptions{
 			DirectoryName: "app",
 			Environment:   append(os.Environ(), "DEV=1"),
 			Program:       options.Bun,
-			Args:          []string{"x", "vite", "build", "--logLevel=info", "--outDir=dist/client", "--emptyOutDir=true", "--watch"},
+			Args:          []string{"x", "vite", "build", "--logLevel=info", "--outDir=dist/client", "--emptyOutDir=false", "--watch"},
 		}) {
 			messages.Error("vite failed to generate dist/client/*")
 		}
@@ -25,7 +31,7 @@ func PackageWatch(options PackageWatchOptions) (err error) {
 			DirectoryName: "app",
 			Environment:   append(os.Environ(), "DEV=1"),
 			Program:       options.Bun,
-			Args:          []string{"x", "vite", "build", "--logLevel=info", "--outDir=dist/server", "--emptyOutDir=true", "--watch", "--ssr=app.server.ts"},
+			Args:          []string{"x", "vite", "build", "--logLevel=info", "--outDir=dist/server", "--emptyOutDir=false", "--watch", "--ssr=app.server.ts"},
 		}) {
 			messages.Error("vite failed to generate dist/server/app.server.js")
 		}
