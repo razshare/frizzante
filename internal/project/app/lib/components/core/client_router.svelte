e3faaa548b72d0c4a43b1e545fa64b69702b7f30
Razvan <razvan@razshare.dev>
2026-02-20 21:42:04 +0100

    chore(app): fixing svelte linting errors.

--- internal/project/app/lib/components/core/client_router.svelte
+++ internal/project/app/lib/components/core/client_router.svelte
@@ -1,36 +1,18 @@
 <script lang="ts">
-    import { setContext, type SvelteComponent } from "svelte"
-    import { views } from "$exports.client"
+    import { setContext, type Component } from "svelte"
+    import { views } from "$exports.server"
     import type { View } from "$lib/scripts/core/view.d.ts"
     let { name, props, render, align, type }: View<Record<string, unknown>> = $props()
-    const components = views as unknown as Record<string, () => Promise<SvelteComponent>>
-    const view: View<Record<string, unknown>> = $state({
-        name,
-        props,
-        render,
-        align,
-        type,
-        async pin() {
-            pending.component = await components[view.name]()
-            pending.props = $state.snapshot(view.props)
-        },
-    })
+    const components = views as unknown as Record<string, Component>
+    // svelte-ignore state_referenced_locally
+    const viewStateValue = { name, props, render, align, type, async pin() {} }
+    const view: View<Record<string, unknown>> = $state(viewStateValue)
     setContext("view", view)
-    const pending = {
-        component: false as false | SvelteComponent,
-        props: {} as Record<string, unknown>,
-    }
 </script>
 
 {#each Object.keys(components) as key (key)}
     {#if key === view.name}
-        {#await components[key]()}
-            {#if pending.component}
-                {@const Component = pending.component}
-                <Component.default {...pending.props} />
-            {/if}
-        {:then Component}
-            <Component.default {...view.props} />
-        {/await}
+        {@const Component = components[key]}
+        <Component {...view.props} />
     {/if}
 {/each}
