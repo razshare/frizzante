ca07eebe8d20480675adb17d27f21dabf543aec7
Razvan <razvan@razshare.dev>
2026-03-13 02:29:32 +0100

    feat(plugins): ali plugins now receive flags and parameters as arguments.

--- cli/menus/load_plugin.go
+++ cli/menus/load_plugin.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/BurntSushi/toml"
 	"github.com/razshare/frizzante/cli/apps"
@@ -12,14 +13,14 @@ import (
 	"github.com/razshare/frizzante/tui/search"
 )
 
-func LoadPlugins(menu *Menu, directory string) (err error) {
+func LoadPlugins(menu *Menu, directoryName string) (err error) {
 	type Configuration struct {
 		Description string `toml:"description"`
 		Hidden      bool   `toml:"hidden"`
 	}
-	if files.IsDirectory(directory) {
+	if files.IsDirectory(directoryName) {
 		var fileNames []string
-		if fileNames, err = files.ReadDirectory(directory); err != nil {
+		if fileNames, err = files.ReadDirectory(directoryName); err != nil {
 			return
 		}
 		for _, fileName := range fileNames {
@@ -41,10 +42,48 @@ func LoadPlugins(menu *Menu, directory string) (err error) {
 				Choice: search.Choice{Id: id, Description: configuration.Description},
 				Active: func(menu *Menu, app apps.App, value string, query []string) bool { return value == id },
 				Handle: func(menu *Menu, app apps.App, value string, query []string) (err error) {
+					parameters := make([]string, 0)
+					if *app.Strict {
+						parameters = append(parameters, "--strict")
+					}
+					if *app.Incremental {
+						parameters = append(parameters, "--incremental")
+					}
+					if *app.Bun != "" {
+						parameters = append(parameters, fmt.Sprintf("--bun=\"%s\"", *app.Bun))
+					}
+					if *app.Go != "" {
+						parameters = append(parameters, fmt.Sprintf("--go=\"%s\"", *app.Go))
+					}
+					if *app.Tags != "" {
+						parameters = append(parameters, fmt.Sprintf("--tags=\"%s\"", *app.Tags))
+					}
+					if *app.Air != "" {
+						parameters = append(parameters, fmt.Sprintf("--air=\"%s\"", *app.Air))
+					}
+					if *app.Database != "" {
+						parameters = append(parameters, fmt.Sprintf("--database=\"%s\"", *app.Database))
+					}
+					if *app.DatabaseType != "" {
+						parameters = append(parameters, fmt.Sprintf("--database-type=\"%s\"", *app.DatabaseType))
+					}
+					if *app.Sqlc != "" {
+						parameters = append(parameters, fmt.Sprintf("--sqlc=\"%s\"", *app.Sqlc))
+					}
+					if *app.SqlcYaml != "" {
+						parameters = append(parameters, fmt.Sprintf("--sqlc-yaml=\"%s\"", *app.SqlcYaml))
+					}
+					parameters = append(parameters, value)
+					parameters = append(parameters, query...)
+					packageDirectoryName := fmt.Sprintf(".%s%s", string(filepath.Separator), packageName)
+					goRun := []string{"run", packageDirectoryName}
+					if len(parameters) > 0 {
+						goRun = append(goRun, strings.Join(parameters, " "))
+					}
 					if !messages.Command(messages.CommandOptions{
 						Environment: os.Environ(),
 						Program:     *app.Go,
-						Args:        []string{"run", fmt.Sprintf(".%s%s", string(filepath.Separator), packageName)},
+						Args:        goRun,
 					}) {
 						err = fmt.Errorf("plugin %s failed to execute", packageName)
 						return
