5eff140bf6d4eb0f3384a6a6db394fdc9eb2a09c
Chris Joseph <95969273+cmjoseph07@users.noreply.github.com>
2026-03-24 20:07:55 -0500

    fix(send): improve error messages in WsUpgradeWithUpgrader to better reflect the issue

--- internal/project/lib/core/send/ws.go
+++ internal/project/lib/core/send/ws.go
@@ -18,13 +18,21 @@ func WsUpgrade(client *clients.Client) {
 func WsUpgradeWithUpgrader(client *clients.Client, upgrader websocket.Upgrader) {
 	conn, err := upgrader.Upgrade(client.Writer, &client.Request, nil)
 	if err != nil {
-		client.Options.ErrorLog.Println(err, stack.Trace())
+		client.Options.ErrorLog.Printf(
+			"send.WsUpgradeWithUpgrader: failed to upgrade to WebSocket: %v\n%s",
+			err,
+			stack.Trace(),
+		)
 		return
 	}
 
 	defer func() {
 		if cerr := conn.Close(); cerr != nil {
-			client.Options.ErrorLog.Println(cerr, stack.Trace())
+			client.Options.ErrorLog.Printf(
+				"send.WsUpgradeWithUpgrader: failed to close WebSocket connection: %v\n%s",
+				cerr,
+				stack.Trace(),
+			)
 		}
 	}()
 
