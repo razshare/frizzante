a2a0ded3b69bf369399722c090237232e3050cd5
Razvan <razvan@razshare.dev>
2026-05-10 15:48:12 +0200

    chore(app): fixing eslint.

--- internal/project/app/eslint.config.js
+++ internal/project/app/eslint.config.js
@@ -1,16 +1,11 @@
 import prettier from "eslint-config-prettier"
 import js from "@eslint/js"
-import { includeIgnoreFile } from "@eslint/compat"
 import svelte from "eslint-plugin-svelte"
 import globals from "globals"
-import { fileURLToPath } from "node:url"
 import ts from "typescript-eslint"
 import svelteConfig from "./svelte.config.js"
 
-const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url))
-
 export default ts.config(
-    includeIgnoreFile(gitignorePath),
     js.configs.recommended,
     ...ts.configs.recommended,
     ...svelte.configs.recommended,
@@ -33,4 +28,7 @@ export default ts.config(
             },
         },
     },
+    {
+        ignores: ["dist", "node_modules"],
+    }
 )
