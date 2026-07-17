f07e42c54dbf36ac76efbc419a5fc23f8679d779
Razvan <razvan@razshare.dev>
2026-07-17 10:38:06 +0200

    feat(v2): bump.

--- cli/actions/configure.go
+++ cli/actions/configure.go
@@ -20,9 +20,6 @@ func Configure(options ConfigureOptions) (err error) {
 	if err = Install(InstallOptions{Go: options.Go, Bun: options.Bun}); err != nil {
 		return
 	}
-	if err = generations.Types(generations.TypesOptions{Go: options.Go, Tags: options.Tags}); err != nil {
-		return
-	}
 	if err = Package(PackageOptions{Go: options.Go, Bun: options.Bun}); err != nil {
 		return
 	}
