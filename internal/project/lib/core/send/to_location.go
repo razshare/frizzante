e7afcaa34b81fa06f33d0fb055cf466a7d25c2b3
Razvan <razvan@razshare.dev>
2026-07-17 09:11:32 +0200

    feat(v2): fixing send.ToLocation().

--- internal/project/lib/core/send/to_location.go
+++ internal/project/lib/core/send/to_location.go
@@ -2,7 +2,7 @@ package send
 
 import "net/http"
 
-func ToLocation(writer http.ResponseWriter) {
-	writer.Header().Add("Location", "/welcome")
+func ToLocation(writer http.ResponseWriter, location string) {
+	writer.Header().Add("Location", location)
 	writer.WriteHeader(http.StatusSeeOther)
 }
