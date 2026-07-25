d525ae54bcbe34d7a6a2e0b69c08816e8eca706b
Razvan <razvan@razshare.dev>
2026-07-25 16:43:40 +0200

    chore(app): fixing migration generation.

--- cli/generations/migration.go
+++ cli/generations/migration.go
@@ -1,42 +1,17 @@
 package generations
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/razshare/frizzante/internal/project/lib/core/files"
-	"github.com/razshare/frizzante/tui/inputs"
 	"github.com/razshare/frizzante/tui/messages"
-	"github.com/razshare/frizzante/tui/search"
-	"github.com/razshare/frizzante/tui/select_one"
 	"github.com/razshare/frizzante/tui/spinners"
 )
 
 func Migration(options MigrationOptions) (err error) {
-	sqlcYaml := options.SqlcYaml
-	if sqlcYaml == "" {
-		if options.Strict {
-			err = errors.New("no sqlc.yaml file provided")
-			return
-		}
-		var names []string
-		if names, err = files.FindWithSuffix("lib", "sqlc.yaml"); err != nil {
-			return
-		}
-		choices := make([]search.Choice, len(names))
-		for index, name := range names {
-			choices[index] = search.Choice{Id: name}
-		}
-		sqlcYaml, err = select_one.Sendf(choices, "where is your sqlc.yaml file located?")
-		if sqlcYaml == "other" {
-			if sqlcYaml, err = inputs.Send("where is your sqlc.yaml file located?"); err != nil {
-				return
-			}
-		}
-	}
 	type Configuration struct {
 		Sql []struct {
 			Schema string `yaml:"schema"`
