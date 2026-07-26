a3890562b2464a1afd72a2cc8e054310558b4c69
Razvan <razvan@razshare.dev>
2026-07-26 12:10:29 +0200

    chore(app): fixing routes and guards handler type aliasing.

--- internal/project/lib/core/guards/handler.go
+++ internal/project/lib/core/guards/handler.go
@@ -6,4 +6,4 @@ import (
 	"github.com/razshare/frizzante/v2/internal/project/lib/core/scopes"
 )
 
-type Handler = func(scope scopes.Scope, request *http.Request, writer http.ResponseWriter, allow func())
+type Handler func(scope scopes.Scope, request *http.Request, writer http.ResponseWriter, allow func())
--- internal/project/lib/core/routes/handler.go
+++ internal/project/lib/core/routes/handler.go
@@ -6,4 +6,4 @@ import (
 	"github.com/razshare/frizzante/v2/internal/project/lib/core/scopes"
 )
 
-type Handler = func(scope scopes.Scope, request *http.Request, writer http.ResponseWriter)
+type Handler func(scope scopes.Scope, request *http.Request, writer http.ResponseWriter)
